01d4f1b21d20f1d7ef75c91bb104a7437dd33e66
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-04-09 10:20:09 +0100

    add reset button, global state

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,17 +1,27 @@
 import CreditsCounter from '@/components/CreditsCounter';
 import ToggleClicks from '@/components/ToggleClicks';
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+  const [resetState, setResetState] = useState(false);
+
+  const handleReset = () => {
+    setResetState((prev) => !prev);
+  };
+
   return (
     <View style={styles.homeContainer}>
       <View style={[styles.playerContainer, styles.farContainer]}>
-        <ToggleClicks userRole={'runner'} />
-        <CreditsCounter playerID="player2" />
+        <ToggleClicks userRole={'runner'} reset={resetState} />
+        <CreditsCounter playerID="player2" reset={resetState} />
       </View>
+      <TouchableOpacity style={styles.resetButton} onPress={handleReset}>
+        <Text style={styles.resetButtonText}>Reset</Text>
+      </TouchableOpacity>
       <View style={styles.playerContainer}>
-        <ToggleClicks userRole={'corp'} />
-        <CreditsCounter playerID="player1" />
+        <ToggleClicks userRole={'corp'} reset={resetState} />
+        <CreditsCounter playerID="player1" reset={resetState} />
       </View>
     </View>
   );
@@ -28,5 +38,17 @@ const styles = StyleSheet.create({
   },
   farContainer: {
     transform: [{ rotate: '180deg' }],
+  },
+  resetButton: {
+    backgroundColor: '#FF0000',
+    padding: 10,
+    margin: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  resetButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });

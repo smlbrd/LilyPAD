7605ceed0cb2b75e0471d9cb94362cba0d2b1d7d
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-09-07 09:23:55 +0100

    LP3 add reset to credit counter

--- components/CreditCounter.tsx
+++ components/CreditCounter.tsx
@@ -1,20 +1,32 @@
-import Counter from './Counter';
+import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import Counter from './Counter';
 import CreditIcon from '../assets/NSG_CREDIT.svg';
+import { useReset } from '../contexts/ResetContext';
 
-const CreditCounter = () => (
-  <Counter
-    value={5}
-    min={0}
-    renderDisplay={(credits) => (
-      <View className="pointer-events-none absolute left-0 right-0 z-10 h-full flex-row items-center justify-center">
-        <CreditIcon width={50} height={50} fill="#FFF" />
-        <View className="w-10">
-          <Text className="text-nowrap text-center text-4xl font-bold text-white">{credits}</Text>
+const CreditCounter = () => {
+  const [credits, setCredits] = useState(5);
+  const { resetCount } = useReset();
+
+  useEffect(() => {
+    setCredits(5);
+  }, [resetCount]);
+
+  return (
+    <Counter
+      value={credits}
+      min={0}
+      onChange={setCredits}
+      renderDisplay={(credits) => (
+        <View className="pointer-events-none absolute left-0 right-0 z-10 h-full flex-row items-center justify-center">
+          <CreditIcon width={50} height={50} fill="#FFF" />
+          <View className="w-10">
+            <Text className="text-nowrap text-center text-4xl font-bold text-white">{credits}</Text>
+          </View>
         </View>
-      </View>
-    )}
-  />
-);
+      )}
+    />
+  );
+};
 
 export default CreditCounter;
--- components/__tests__/CreditCounter.test.tsx
+++ components/__tests__/CreditCounter.test.tsx
@@ -1,5 +1,7 @@
 import { fireEvent, render } from '@testing-library/react-native';
 import CreditCounter from '../CreditCounter';
+import { ResetProvider } from '../../contexts/ResetContext';
+import ResetButton from '../ResetButton';
 
 describe('CreditCounter', () => {
   it('renders the initial value', () => {
@@ -9,13 +11,17 @@ describe('CreditCounter', () => {
 
   it('increments the credit value', () => {
     const { getByText } = render(<CreditCounter />);
-    fireEvent.press(getByText('+'));
+    const plus = getByText('+');
+
+    fireEvent.press(plus);
     expect(getByText('6')).toBeTruthy();
   });
 
   it('decrements the credit value', () => {
     const { getByText } = render(<CreditCounter />);
-    fireEvent.press(getByText('-'));
+    const minus = getByText('-');
+
+    fireEvent.press(minus);
     expect(getByText('4')).toBeTruthy();
   });
 
@@ -23,12 +29,35 @@ describe('CreditCounter', () => {
     const { getByText } = render(<CreditCounter />);
     expect(getByText('5')).toBeTruthy();
 
+    const minus = getByText('-');
     for (let i = 0; i < 5; i++) {
-      fireEvent.press(getByText('-'));
+      fireEvent.press(minus);
     }
     expect(getByText('0')).toBeTruthy();
 
-    fireEvent.press(getByText('-'));
+    fireEvent.press(minus);
     expect(getByText('0')).toBeTruthy();
+  });
+
+  it('resets clicks when reset is triggered', async () => {
+    const { findByText, getByTestId, getByText } = render(
+      <ResetProvider>
+        <CreditCounter />
+        <ResetButton />
+      </ResetProvider>
+    );
+    expect(getByText('5')).toBeTruthy();
+
+    const plus = getByText('+');
+
+    fireEvent.press(plus);
+    expect(getByText('6')).toBeTruthy();
+
+    fireEvent.press(getByTestId('reset-button'));
+
+    const confirmResetButton = await findByText('Reset');
+    fireEvent.press(confirmResetButton);
+
+    expect(getByText('5')).toBeTruthy();
   });
 });
